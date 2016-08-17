5858bd8718c4ba8c86827e82742cbb08d1c86a77
Ilya notebook <ilya.kugaevsky@gmail.com>
2016-08-17 12:00:37 +0700

    start to manage Tour view

--- Predictions/Controllers/ToursController.cs
+++ Predictions/Controllers/ToursController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Predictions.Models;
+using System.Net;
 
 namespace Predictions.Controllers
 {
@@ -14,10 +16,40 @@ namespace Predictions.Controllers
         {
             using (var context = new PredictionsContext())
             {
+                //inicialization
+
+                //for(int i = 1; i <= 8; i++)
+                //{
+                //    Tour tour = new Tour()
+                //    {
+                //        TourId = i,
+                //        StartDate = new DateTime()
+                //    };
+                //    context.Tours.Add(tour);
+                //}
+                //context.SaveChanges();
+
                 var tours = context.Tours.ToList();
                 return View(tours);
             }
 
+        }
+
+        public ActionResult EditTour(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var context = new PredictionsContext())
+            {
+                Tour tour = context.Tours.Find(id);
+                if (tour == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(tour);
+            }
         }
     }
 }
