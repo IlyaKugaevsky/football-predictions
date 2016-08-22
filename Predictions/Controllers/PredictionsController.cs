3fb48c304f6f1439f1cbda6b77087e410ddb1902
Ilya PC <ilya.kugaevsky@gmail.com>
2016-08-23 01:27:48 +0700

    predictions output. works bad

--- Predictions/Controllers/PredictionsController.cs
+++ Predictions/Controllers/PredictionsController.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Predictions.Models;
 using Predictions.ViewModels;
+using System.Net;
+using System.Data.Entity;
 
 namespace Predictions.Controllers
 {
@@ -27,12 +30,33 @@ namespace Predictions.Controllers
             }
         }
 
+        [HttpPost]
         public ActionResult Index(PredictionsDisplayViewModel viewModel)
         {
             using (var context = new PredictionsContext())
             {
-                var expertlist = context.Experts.ToList();
-                var tourlist = context.Tours.ToList();
+                //var expert = context.Experts.Find
+
+                //test!
+                viewModel.SelectedExpertId = 1;
+                viewModel.Tourlist = context.Tours.ToList();
+
+                var tour = context.Tours
+                    .Include(t => t.Matches
+                        .Select(m => m.HomeTeam))
+                    .Include(t => t.Matches
+                        .Select(m => m.AwayTeam))
+                    .Include(t => t.Matches
+                        .Select(m => m.Predictions
+                            /*.SingleOrDefault(p => p.ExpertId == viewModel.SelectedExpertId)*/))
+                    .SingleOrDefault(t => t.TourId == viewModel.SelectedTourId);
+
+                if (tour == null)
+                {
+                    return HttpNotFound();
+                }
+
+                viewModel.Tour = tour;
 
                 return View(viewModel);
             }
--- Predictions/ViewModels/PredictionsDisplayViewModel.cs
+++ Predictions/ViewModels/PredictionsDisplayViewModel.cs
@@ -12,11 +12,9 @@ namespace Predictions.ViewModels
         public List<Expert> Expertlist { get; set; }
         public List<Tour> Tourlist { get; set; }
 
+        public Tour Tour { get; set; } //tour + matches + teams + predictions
+
         public int SelectedExpertId { get; set; }
         public int SelectedTourId { get; set; }
-
-        public int HomeTeamId { get; set; }
-        public int AwayTeamId { get; set; }
-        public DateTime Date { get; set; }
     }
 }
