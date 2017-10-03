374d9c9ef06e4257837743b15e49ad7f364d3249
Ilya PC <ilya.kugaevsky@gmail.com>
2017-10-03 20:04:18 +0700

    Castle Windsor IoC-container presets, fix custom ViewEngine namespace

--- src/Predictions/FrontEnd/Views/CustomViewEngine.cs
+++ src/Predictions/FrontEnd/Views/CustomViewEngine.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace Predictions.Views
+namespace Predictions.FrontEnd.Views
 {
     public class CustomViewEngine: RazorViewEngine
     {
--- src/Predictions/Global.asax.cs
+++ src/Predictions/Global.asax.cs
@@ -5,19 +5,32 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Predictions.Views;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using Predictions.FrontEnd.Views;
+using Predictions.Windsor.Plumbing;
+
 
 namespace Predictions
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static IWindsorContainer _container;
+
+        private static void BootstrapContainer()
+        {
+            _container = new WindsorContainer()
+                .Install(FromAssembly.This());
+            var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
             ViewEngines.Engines.Add(new CustomViewEngine());
         }
     }
